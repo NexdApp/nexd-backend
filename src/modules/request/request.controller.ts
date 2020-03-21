f80741d0c493ce2441658b8e34c13569f8d92a6c
Vincent Cui <wenvincent.cui@gmail.com>
2020-03-21 23:54:52 +0100

    fix relative paths

--- src/modules/request/request.controller.ts
+++ src/modules/request/request.controller.ts
@@ -11,9 +11,9 @@ import { ApiBearerAuth, ApiCreatedResponse, ApiTags } from '@nestjs/swagger';
 import { RequestService } from './request.service';
 import { Request as RequestEntity } from './request.entity';
 import { CreateRequestDto } from './dto/create-request.dto';
-import { ReqUser } from 'modules/common/decorators/user.decorator';
-import { User } from 'modules/user/user.entity';
-import { JwtAuthGuard } from 'modules/common/guards/jwt-guard';
+import { ReqUser } from '../common/decorators/user.decorator';
+import { User } from '../user/user.entity';
+import { JwtAuthGuard } from '../common/guards/jwt-guard';
 
 @ApiBearerAuth()
 @ApiTags('Request')
