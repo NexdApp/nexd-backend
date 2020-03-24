65bed109f114d5a8b0eebfb63f113e89f25bb725
Andre Stefanov <andre.stefanov@jambit.com>
2020-03-24 13:08:10 +0100

    added server data to swagger docs (fixed #31)

--- src/swagger/index.ts
+++ src/swagger/index.ts
@@ -1,12 +1,18 @@
-import {INestApplication} from '@nestjs/common';
-import {DocumentBuilder, SwaggerModule} from '@nestjs/swagger';
-import {SWAGGER_API_CURRENT_VERSION, SWAGGER_API_DESCRIPTION, SWAGGER_API_NAME, SWAGGER_API_ROOT} from './constants';
+import { INestApplication } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import {
+  SWAGGER_API_CURRENT_VERSION,
+  SWAGGER_API_DESCRIPTION,
+  SWAGGER_API_NAME,
+  SWAGGER_API_ROOT,
+} from './constants';
 
 export const setupSwagger = (app: INestApplication) => {
   const options = new DocumentBuilder()
     .setTitle(SWAGGER_API_NAME)
     .setDescription(SWAGGER_API_DESCRIPTION)
     .setVersion(SWAGGER_API_CURRENT_VERSION)
+    .addServer('http://nexd-api-alb-1107636132.eu-central-1.elb.amazonaws.com/')
     .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, options);
