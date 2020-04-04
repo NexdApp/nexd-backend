1b62ab6df4422393ef6f9894048639086f7a213b
Vincent Cui <wenvincent.cui@gmail.com>
2020-04-05 00:37:35 +0200

    add apiproperty to number attributes

--- src/modules/calls/dto/call-query.dto.ts
+++ src/modules/calls/dto/call-query.dto.ts
@@ -1,10 +1,18 @@
 import { ApiProperty } from '@nestjs/swagger';
 
 export class CallQueryDto {
+  @ApiProperty({
+    description: '',
+    type: 'integer',
+  })
   amount?: number;
 
   country?: string;
 
+  @ApiProperty({
+    description: '',
+    type: 'integer',
+  })
   zip?: number;
 
   city?: string;
