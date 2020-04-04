f064d5292e403b4c18e06489b3ff83c97043fc7a
Vincent Cui <wenvincent.cui@gmail.com>
2020-04-05 00:13:58 +0200

    add user authentication

--- src/modules/calls/calls.controller.ts
+++ src/modules/calls/calls.controller.ts
@@ -10,6 +10,7 @@ import {
   Put,
   ClassSerializerInterceptor,
   Post,
+  UseGuards,
 } from '@nestjs/common';
 import {
   ApiResponse,
@@ -23,6 +24,7 @@ const VoiceResponse = require('twilio').twiml.VoiceResponse;
 import { CallsService } from './calls.service';
 import { CallQueryDto } from './dto/call-query.dto';
 import { ConfigurationService } from 'src/configuration/configuration.service';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 
 
 @Controller('call')
@@ -30,7 +32,8 @@ import { ConfigurationService } from 'src/configuration/configuration.service';
 @ApiUnauthorizedResponse({ description: 'Unauthorized' })
 export class CallsController {
   constructor(private readonly callService: CallsService,
-    private readonly configService: ConfigurationService) { }
+    private readonly configService: ConfigurationService
+  ) { }
 
   @Post('twilio/call')
   @ApiResponse({ status: HttpStatus.ACCEPTED, description: 'Success' })
@@ -65,6 +68,7 @@ export class CallsController {
     res.status(200).send('Successful');
   }
 
+  @UseGuards(JwtAuthGuard)
   @Put('converted/:sid')
   @ApiResponse({ status: HttpStatus.ACCEPTED, description: 'Success' })
   @ApiResponse({ status: HttpStatus.BAD_REQUEST, description: 'Bad Request' })
@@ -83,6 +87,7 @@ export class CallsController {
     }
   }
 
+  @UseGuards(JwtAuthGuard)
   @UseInterceptors(ClassSerializerInterceptor)
   @Get('calls')
   @ApiResponse({ status: HttpStatus.ACCEPTED, description: 'Success' })
@@ -92,6 +97,7 @@ export class CallsController {
     return await this.callService.queryCalls(body);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get('calls/:sid/record')
   @ApiResponse({ status: HttpStatus.ACCEPTED, description: 'Success' })
   @ApiResponse({ status: HttpStatus.BAD_REQUEST, description: 'Bad Request' })
@@ -100,6 +106,7 @@ export class CallsController {
     return await this.callService.getCallRecord(sid);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get('calls/:sid/transcription')
   @ApiResponse({ status: HttpStatus.ACCEPTED, description: 'Success' })
   @ApiResponse({ status: HttpStatus.BAD_REQUEST, description: 'Bad Request' })
