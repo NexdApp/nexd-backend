d9c7632ea5c9ae28648529e58dd3850dbad1f4cb
Lukas Huth <2001Kruemel@gmail.com>
2020-04-11 01:08:34 +0200

    Added LocationInfoQuery object
    update getLocationInfo() parameters and renamed migration script

---
+++ src/modules/locationInfos/dto/locationInfoQuery.dto.ts
@@ -0,0 +1,16 @@
+
+export class LocationInfoQuery {
+  zip?: string;
+
+  country?: string;
+
+  city?: string;
+
+  state?: string;
+
+  stateShort?: string;
+
+  area?: string;
+
+  location?: { type: string, coordinates: number[] };
+}
--- src/modules/locationInfos/locationInfos.service.ts
+++ src/modules/locationInfos/locationInfos.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { LocationInfo } from './locationInfo.entity';
 import { Repository } from 'typeorm';
+import { LocationInfoQuery } from './dto/locationInfoQuery.dto';
 
 @Injectable()
 export class LocationInfosService {
@@ -18,22 +19,19 @@ export class LocationInfosService {
    * @param zip 
    * @param location 
    */
-  async getLocationInfo(city?: string,
-    country?: string,
-    zip?: string,
-    location?: { longitude: number, latitude: number }): Promise<LocationInfo> {
+  async getLocationInfo(locationQuery: LocationInfoQuery): Promise<LocationInfo> {
 
     const query = this.localInfoRepo.createQueryBuilder();
 
-    if (city)
-      query.andWhere("LocationInfo.city = :city", { city: city });
-    if (country)
-      query.andWhere("LocationInfo.city = :city", { country: country });
-    if (zip)
-      query.andWhere("LocationInfo.zip = :zip", { zip: city });
-    if (location) {
-      query.andWhere("LocationInfo.latitude = :latitude", { latitude: location.latitude });
-      query.andWhere("LocationInfo.longitude = :longitude", { longitude: location.longitude });
+    if (locationQuery.city)
+      query.andWhere("LocationInfo.city = :city", { city: locationQuery.city });
+    if (locationQuery.country)
+      query.andWhere("LocationInfo.city = :city", { country: locationQuery.country });
+    if (locationQuery.zip)
+      query.andWhere("LocationInfo.zip = :zip", { zip: locationQuery.city });
+    if (locationQuery.location) {
+      query.andWhere("LocationInfo.latitude = :latitude", { latitude: locationQuery.location.coordinates[0] });
+      query.andWhere("LocationInfo.longitude = :longitude", { longitude: locationQuery.location.coordinates[1] });
     }
 
     return await query.getOne();
