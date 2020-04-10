0f49dab2fe6a2e2e1f6c56ee8a2e6d9116ba2726
Lukas Huth <2001Kruemel@gmail.com>
2020-04-11 01:31:02 +0200

    Added "migrate:local-infos" npm script

--- src/modules/locationInfos/data/migration.py
+++ src/modules/locationInfos/data/migration.py
@@ -3,19 +3,21 @@ import json
 import os
 
 sql_insert: str = """INSERT INTO "locationInfos" (area, city, country, state, "stateShort", zip, location) VALUES ('{0}', '{1}', '{2}', '{3}', '{4}', '{5}', ST_GeogFromText('SRID=4326;POINT({6} {7})'));\n"""
+data_path: str = os.getcwd() + "/src/modules/locationInfos/data/"
+
 
 if __name__ == "__main__":
-    files = os.listdir(os.getcwd())
+    files = os.listdir(data_path)
 
     for filename in files:
         if filename.split(".")[1:][0] == "txt":
-            count = 0
+            count: int = 0
             data = []
-            with open(filename, "r") as file:
+            with open(data_path + filename, "r") as file:
                 line = file.readline()
 
                 while line:
-                    lineData = line.split("\t")
+                    lineData: str = line.split("\t")
 
                     data.append({
                         "country": lineData[0],
@@ -31,7 +33,7 @@ if __name__ == "__main__":
                     count = count + 1
                     line = file.readline()
 
-            with open(filename.split(".")[0] + ".sql", "w") as file:
+            with open(data_path + filename.split(".")[0] + ".sql", "w") as file:
                 for entry in data:
                     file.write(sql_insert.format(entry["area"],
                                                  entry["city"],
@@ -42,8 +44,8 @@ if __name__ == "__main__":
                                                  entry["latitude"],
                                                  entry["longitude"]))
 
-            with open(filename.split(".")[0] + ".json", "w") as file:
-                jsonData = json.dumps(
+            with open(data_path + filename.split(".")[0] + ".json", "w") as file:
+                jsonData: str = json.dumps(
                     {"local_infos": data}, indent=2, sort_keys=True, ensure_ascii=False)
 
                 file.write(jsonData)
@@ -52,7 +54,7 @@ if __name__ == "__main__":
                                                          filename.split(".")[0] + ".json", filename.split(".")[0] + ".sql", count))
 
             os.system(
-                "docker cp {} postgis-nexd:/".format(filename.split(".")[0] + ".sql"))
+                "docker cp {} postgis-nexd:/".format(data_path + filename.split(".")[0] + ".sql"))
 
-            os.system("docker exec -it postgis-nexd psql -U username -d dbname -f {}".format(
+            os.system("docker exec -it postgis-nexd psql -U username -d dbname -f {} > /dev/null".format(
                 filename.split(".")[0] + ".sql"))
