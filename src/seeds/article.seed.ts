7140ee97c33592bb3b7a6b79274a0440e1fe3ade
udaychauhan <uday.myplanet@gmail.com>
2020-05-04 21:41:52 +0530

    seeding 10 articles by name only

--- src/seeds/article.seed.ts
+++ src/seeds/article.seed.ts
@@ -3,11 +3,15 @@ import { Article } from '../modules/articles/article.entity';
 import { Connection } from 'typeorm';
 export class ArticleSeed implements Seeder {
   public async run(factory: Factory, connection: Connection): Promise<void> {
+    let articles: { name: string }[] = [];
+    for (let i = 1; i <= 10; i++) {
+      articles.push({ name: 'article_' + i });
+    }
     await connection
       .createQueryBuilder()
       .insert()
       .into(Article)
-      .values([{ name: 'article' }])
+      .values(articles)
       .execute();
   }
 }
