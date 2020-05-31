e4675f1c969ef723efdba1c1319268f54de28daf
Fabian Nickel <mail@fabiannickel.de>
2020-05-31 23:58:59 +0200

    another wrong path, fix test

--- src/migrations/1590960420422-UnitValues.ts
+++ src/migrations/1590960420422-UnitValues.ts
@@ -1,6 +1,6 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 import { UnitsSeed } from '../seed/units.seed';
-import { Unit } from 'src/modules/articles/unit.entity';
+import { Unit } from '../modules/articles/unit.entity';
 
 export class UnitValues1590960420422 implements MigrationInterface {
   name = 'UnitValues1590960420422';
